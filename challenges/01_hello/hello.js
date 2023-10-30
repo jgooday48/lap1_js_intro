ca96b2de1d6807cb12b8290dd2856ff6e72cd994
jgooday48 <joelgooday48@gmail.com>
2023-10-30 17:00:16 +0000

    add capitalise.js for joel

--- challenges/01_hello/hello.js
+++ challenges/01_hello/hello.js
@@ -1,6 +1,15 @@
 
 // Do not delete the code below 🙏
-
+function hello(name) {
+  if (name.length === 0) {
+    return 'no empty strings allowed'
+  }
+ else if (typeof name === null) {
+    return 'Hello Stranger'
+  }
+  else{
+  return `Hello ${name}`
+}}
 module.exports = {
   hello
 }
--- challenges/03_capitalise/capitalise.js
+++ challenges/03_capitalise/capitalise.js
@@ -1,4 +1,22 @@
 const capitalise = (input) => {
+  if (input === '') {
+    return input
+  }
+
+
+
+   else if (input[0] === input[0].toUpperCase()){
+    return input[0] + input.slice(1).toLowerCase()
+  }
+  else {
+    let first = input[0]
+    const rest = input.slice(1)
+
+  const restlower = rest.toLowerCase()
+  const firstUpper = first.toUpperCase()
+  const word = firstUpper + restlower
+  return word}
+
 
 }
 
